2addcc82a5c94dbba1fcee0d8254284d00666143
Alp Karar <88462979+AlpKarar@users.noreply.github.com>
2022-02-15 18:51:56 +0300

    Create solution.java

---
+++ solution.java
@@ -0,0 +1,30 @@
+/*
+  Ödev
+  Java ile basamak sayısının kullanıcıdan alınan ve döngüler kullanılarak, yıldızlar(*) ile ekrana ters üçgen çizen programı yazın.
+*/
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) {
+        
+        int num;
+        
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        num = input.nextInt();
+        
+        for (int i=0; i<num; i++) {
+            for (int j=0; j<i + 1; j++) {
+                System.out.print(" ");
+            }
+            
+            for (int k=0; k<2*(num - i) - 1; k++ ) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
